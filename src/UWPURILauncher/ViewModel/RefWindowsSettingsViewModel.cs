fcda4aeb33384626e31c6e0a7fb69d9c7e87ab97
Edi Wang <Edi.Wang@outlook.com>
2018-06-16 15:16:56 +0800

    refactor code

--- src/UWPURILauncher/ViewModel/RefWindowsSettingsViewModel.cs
+++ src/UWPURILauncher/ViewModel/RefWindowsSettingsViewModel.cs
@@ -14,64 +14,19 @@ namespace UWPURILauncher.ViewModel
 
         private void AddUriReferences()
         {
-            var cn = ReferencesContext.Instance.AllReferences.FirstOrDefault(r => r.Name == "Cortana");
-            if (null != cn)
-            {
-                AddCategoryData(cn.Name, cn.UriReferences.ToList());
-            }
+            var includes = new[] { "Cortana", "Windows Settings", "Devices",
+                                   "Network & Internet", "Accounts", "Apps",
+                                   "Time and language", "Ease of Access", "Privacy",
+                                   "Update & security" };
 
-            var ws = ReferencesContext.Instance.AllReferences.FirstOrDefault(r => r.Name == "Windows Settings");
-            if (null != ws)
+            var rf = ReferencesContext.Instance.AllReferences.Where(r => includes.Contains(r.Name));
+            var categories = rf as Category[] ?? rf.ToArray();
+            if (categories.Any())
             {
-                AddCategoryData(ws.Name, ws.UriReferences.ToList());
-            }
-
-            var ds = ReferencesContext.Instance.AllReferences.FirstOrDefault(r => r.Name == "Devices");
-            if (null != ds)
-            {
-                AddCategoryData(ds.Name, ds.UriReferences.ToList());
-            }
-
-            var ni = ReferencesContext.Instance.AllReferences.FirstOrDefault(r => r.Name == "Network & Internet");
-            if (null != ni)
-            {
-                AddCategoryData(ni.Name, ni.UriReferences.ToList());
-            }
-
-            var ac = ReferencesContext.Instance.AllReferences.FirstOrDefault(r => r.Name == "Accounts");
-            if (null != ac)
-            {
-                AddCategoryData(ac.Name, ac.UriReferences.ToList());
-            }
-
-            var ap = ReferencesContext.Instance.AllReferences.FirstOrDefault(r => r.Name == "Apps");
-            if (null != ap)
-            {
-                AddCategoryData(ap.Name, ap.UriReferences.ToList());
-            }
-
-            var tl = ReferencesContext.Instance.AllReferences.FirstOrDefault(r => r.Name == "Time and language");
-            if (null != tl)
-            {
-                AddCategoryData(tl.Name, tl.UriReferences.ToList());
-            }
-
-            var ea = ReferencesContext.Instance.AllReferences.FirstOrDefault(r => r.Name == "Ease of Access");
-            if (null != ea)
-            {
-                AddCategoryData(ea.Name, ea.UriReferences.ToList());
-            }
-
-            var pv = ReferencesContext.Instance.AllReferences.FirstOrDefault(r => r.Name == "Privacy");
-            if (null != pv)
-            {
-                AddCategoryData(pv.Name, pv.UriReferences.ToList());
-            }
-
-            var us = ReferencesContext.Instance.AllReferences.FirstOrDefault(r => r.Name == "Update & security");
-            if (null != us)
-            {
-                AddCategoryData(us.Name, us.UriReferences.ToList());
+                foreach (var category in categories)
+                {
+                    AddCategoryData(category.Name, category.UriReferences.ToList());
+                }
             }
         }
     }
